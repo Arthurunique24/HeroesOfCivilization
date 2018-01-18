4cdcd9fac0d56871d4e665594a3f02ea453391d2
Vlad Sokolov <sokol.metal@gmail.com>
2018-01-18 14:41:30 +0300

    upd

---
+++ cmake-build-debug/CMakeFiles/CMakeFiles/HeroesOfCivilization.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HeroesOfCivilization.dir/src/main.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/Map.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/MapCell.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/Game.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/Camera.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/GameState.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/PauseState.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/StateManager.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/MenuState.cpp.o"
+  "CMakeFiles/HeroesOfCivilization.dir/src/State.cpp.o"
+  "HeroesOfCivilization.pdb"
+  "HeroesOfCivilization"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HeroesOfCivilization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
