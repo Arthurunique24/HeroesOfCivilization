e1778c8b6025ac62068da1a2f9a08199d9f8370a
Vlad Sokolov <sokol.metal@gmail.com>
2018-01-22 17:47:22 +0300

    update1

--- cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/DependInfo.cmake
@@ -4,25 +4,25 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/ActionMenuState.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/ActionMenuState.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/Camera.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Camera.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/Game.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Game.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/GameState.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/GameState.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/GetName.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/GetName.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/Interface/Buildings.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Interface/Buildings.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/Interface/User.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Interface/User.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/Map.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Map.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/MapCell.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/MapCell.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/PauseState.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/PauseState.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/State.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/State.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/StateManager.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/StateManager.cpp.o"
-  "/Users/Arthur/GitHub/HeroesOfCivilization/src/main.cpp" "/Users/Arthur/GitHub/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/main.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/ActionMenuState.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/ActionMenuState.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/Camera.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Camera.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/Game.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Game.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/GameState.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/GameState.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/GetName.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/GetName.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/Interface/Buildings.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Interface/Buildings.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/Interface/User.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Interface/User.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/Map.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/Map.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/MapCell.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/MapCell.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/PauseState.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/PauseState.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/State.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/State.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/StateManager.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/StateManager.cpp.o"
+  "/Users/user/Github/HeroesOfCivilization/src/main.cpp" "/Users/user/Github/HeroesOfCivilization/cmake-build-debug/CMakeFiles/HeroesOfCivilization.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include"
+  "/Library/Frameworks/SFML.framework"
   )
 
 # Targets to which this target links.
